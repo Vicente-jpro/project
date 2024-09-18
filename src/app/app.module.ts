d85ccb2712b39a2bd2827a441774bf89d3b25a46
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-09-18 11:55:54 +0100

    Home page added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello/hello.component';
 import { TecnicaComponent } from './tecnica/tecnica.component';
 import { ClientesModule } from './clientes/clientes.module';
 import { TemplateModule } from './template/template.module';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HelloComponent,
-    TecnicaComponent
+    TecnicaComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
