d7b37af64e86591b87c87c3762ab571fcf75401f
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2024-09-18 13:50:32 +0100

    form added

--- src/app/clientes/clientes-routing.module.ts
+++ src/app/clientes/clientes-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FormComponent } from './form/form.component';
+import { IndexComponent } from './index/index.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'clientes/form', component: FormComponent},
+  {path: 'clientes', component: IndexComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
